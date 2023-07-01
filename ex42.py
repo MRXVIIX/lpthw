a7e546530e61c8ae3d3193b9528f53c9371cd343
MRXVIIX <127699526+MRXVIIX@users.noreply.github.com>
2023-07-01 11:43:28 -0700

    Add files via upload

---
+++ ex42.py
@@ -0,0 +1,78 @@
+## Animal is-a object (yes, sort of confusing) look at the extra credit
+class Animal(object):
+    pass
+
+## Dog is-a Animal
+class Dog(Animal):
+
+    def __init__(self, name):
+        ## Dog has-a name
+        self.name = name
+
+## Cat is-a Animal
+class Cat(Animal):
+
+    def __init__(self, name):
+        ## Cat has-a name
+        self.name = name
+
+## Person is-a object
+class Person(object):
+
+    def __init__(self, name):
+        ## Person has-a name
+        self.name = name
+
+        ## Person has-a pet of some kind
+        self.pet = None
+
+## Employee is-a Person
+class Employee(Person):
+
+    def __init__(self, name, salary):
+        ## super is-a employee hmm what is this strange magic?
+        super(Employee, self).__init__(name)
+        ## super has-a salary
+        self.salary = salary
+
+## Fish is-a object
+class Fish(object):
+    pass
+
+## Salmon is-a Fish
+class Salmon(Fish):
+    pass
+
+## Halibut is-a Fish
+class Halibut(Fish):
+    pass
+
+
+## rover is-a Dog
+rover = Dog("Rover")
+
+## satan is-a Cat
+satan = Cat("Satan")
+
+## mary is-a Person
+mary = Person("Mary")
+
+## mary has-a pet
+mary.pet = satan
+
+## frank is-a Employee
+frank = Employee("Frank", 120000)
+
+## frank has-a pet
+frank.pet = rover
+
+## flipper is-a Fish
+flipper = Fish()
+
+## course is-a Salmon
+crouse = Salmon()
+
+## harry is-a Halibut
+harry = Halibut()
+
+
