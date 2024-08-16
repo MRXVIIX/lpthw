edd697a7750f46738b9f8f2c1dc9e5d44494e2bd
MRXVIIX <127699526+MRXVIIX@users.noreply.github.com>
2024-08-15 21:59:56 -0400

    Add files via upload

--- ex46/projects/skeleton/ex48/lexicon.py
+++ ex46/projects/skeleton/ex48/lexicon.py
@@ -1,7 +1,7 @@
-userInput = raw_input(' >')
-tokens = userInput.split()
+userInput = raw_input(' >') # takes input typed into the shell and stores it into a variable called "userInput"  
+tokens = userInput.split() # split up "userInput" into strings delimited by space, then store them into the "tokens" variable
 
-lexicon = {
+lexicon = { # create dictionary called "lexicon" 
         'north': 'direction',
         'south': 'direction',
         'east': 'direction',
@@ -18,22 +18,26 @@ lexicon = {
         '1234': 'number',
         '91234': 'number',
         'IAS': 'error',
-        'ASDFADFASDF': 'error',
-        }
+        'ASDFADFASDF': 'error', # all of these are taking a word (ex: "north") and pairing it to the corresponding token (ex: "direction")
+        } # ending the list
 
 
 
-def scan(sentence):
-        words = sentence.split()
-        result = []
+def scan(sentence): # create function called "scan" and parameter "sentence"
+        words = sentence.split() # take the "sentence" parameter, and split into strings delimited by space, and put them into the "words" variable
+        result = [] # create empty list called "result"
 
-        for word in words:
-            if word in lexicon:
-                token = lexicon[word]
-            else:
-                token = 'error'
-            result.append((token, word))
+        for word in words: # iterate a for loop with the variable "word" to process each index in the "words" variable
+            try: # try this block, and if an error happens, then go to except
+                number = int(word) # assign function "int()" to convert all string indices from "lexicon" dictionary into integers, to variable "number"
+                result.append(('number', number)) # add tuple containing " 'number' " (string) and "number" (integer) variables in list result
+            except ValueError: # if no indices in the variable "number" can be converted to an integer, then
+                if word in lexicon: # if the variable "word" is a valid index in the "lexicon" dictionary, 
+                    token = lexicon[word] # then match the valid index to the "lexicon" dictionary and assign it to the variable "token"    
+                else: # or else, if index is not in "lexicon" dictionary 
+                    token = 'error' # assign string "error" to variable "token"
+                result.append((token, word)) # add tuple containing "token" and "word" variables in list "result"
 
-        return result
+        return result # "scan" function outputs "result" list
 
 # the scan function should work by "scanning" the word given (ex: "north") and deciding what type of word it is (ex: direction)
